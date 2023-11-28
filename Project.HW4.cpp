99c8e3a81f20f71c4542e54f07e51ea98a0e30a1
faezeh102 <152311004+faezeh102@users.noreply.github.com>
2023-11-28 21:31:30 +0330

    Add files via upload

---
+++ Project.HW4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+main () {
+	
+	float m,n,A,S,M,R;
+	
+	cout<<"please entre 2 decimal numbers"<< "\n" << ":";
+	cin >> m;
+	cout << ":";
+	cin >> n;
+	
+	A=m+n;
+	
+	S=m-n;
+	
+	M=m*n;
+	
+	R=m/n;
+	
+	cout<< "plus=" << A << "\n" << "minus=" << S << "\n" << "multiply=" << M << "\n"<< "divided by="<< R;
+	
+
+}
