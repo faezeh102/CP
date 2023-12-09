57a380ec0ca64a57366e911024c7973fea6e35a2
faezeh102 <152311004+faezeh102@users.noreply.github.com>
2023-12-09 20:41:00 +0330

    Add files via upload

---
+++ ProjectHW5-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+main() {
+	char vowel;
+	
+	cout << " please enter a character: " << " \n ";
+	
+	cin >> vowel;
+	
+	switch (vowel){
+		
+		case 'a':
+			cout << " vowel ";
+			break;
+			
+		case 'e':
+		    cout << " vowel ";
+		    break;
+		    
+		case 'u':
+		    cout << " vowel ";
+		    break;
+		    
+		case 'i':
+		    cout << " vowel ";
+		    break;
+		    
+		case 'o':
+		    cout << " vowel ";
+		
+	}
+}
