84f13201f19f6cc24d72ca52fb3be2c9f1c191a3
faezeh102 <152311004+faezeh102@users.noreply.github.com>
2024-01-16 00:01:14 +0330

    Add files via upload

---
+++ Project HW-7-1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+ main() { 
+ 
+  int m,n,i,p;{
+  
+      cout << "please enter your numbers" << "\n" << ":";
+    cin >> m ;
+    cout << ":";
+    cin	>> n ;
+    p=1;
+    if (m>0 && n>0){
+    for (i=1; i<=n; i++){
+	
+	  p*=m;
+	   
+	 }
+	 cout << " your answer " << p ;
+	 }
+    else {
+    	cout << "error!";
+	}
+	
+	}
+	}
+    	
+ 
+
+
+    
